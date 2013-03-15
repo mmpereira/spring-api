42b2d97a7c38cfedefc034ca0c5457bfad43edcd
Mario Pereira <mario.mpereira@gmail.com>
2013-03-15 15:04:01 +0100

    using the services

--- src/main/java/com/genebio/nextprot/controllers/HelloController.java
+++ src/main/java/com/genebio/nextprot/controllers/HelloController.java
@@ -1,18 +1,20 @@
 package com.genebio.nextprot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.genebio.nextprot.domain.Message;
+import com.genebio.nextprot.service.MessageService;
 
 @Controller("hello")
 public class HelloController {
-
+	@Autowired
+	MessageService messageService;
 	
 	@RequestMapping(value="/say", method=RequestMethod.GET)
 	public Message hello() {
-		Message msg = new Message("hello world");
-		return msg;
+		return this.messageService.getMessage();
 	}
 }
