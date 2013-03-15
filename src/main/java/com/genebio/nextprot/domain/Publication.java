ab6c3a3c6f38dedce09fabacb8458beccf115b8b
Daniel <ddtxra@gmail.com>
2013-03-15 13:41:25 +0100

    adding the date to publication

--- src/main/java/com/genebio/nextprot/domain/Publication.java
+++ src/main/java/com/genebio/nextprot/domain/Publication.java
@@ -1,6 +1,12 @@
 package com.genebio.nextprot.domain;
 
+import java.util.Date;
+
 public class Publication {
+
+	private String author;
+	private String description;
+	private Date date = new Date();
 
 	public String getAuthor() {
 		return author;
@@ -14,9 +20,12 @@ public class Publication {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	private String author;
-	private String description;
 
-	
-	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 }
