d8c3cd4d98ce9b2da2f3e0cbab0f116e17ccfe2c
Daniel <ddtxra@gmail.com>
2013-03-19 13:30:54 +0100

    adding list of publications

--- src/main/java/com/genebio/nextprot/controllers/PublicationController.java
+++ src/main/java/com/genebio/nextprot/controllers/PublicationController.java
@@ -1,5 +1,7 @@
 package com.genebio.nextprot.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +19,11 @@ public class PublicationController {
 	@RequestMapping(value="/pub", method=RequestMethod.GET)
 	public Publication hello() {
 		return publicationService.getPublicationById(6634104);
+	}
+	
+	
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public List<Publication> hell() {
+		return publicationService.getPublicationByTitle("%Correction%");
 	}
 }
