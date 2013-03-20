e23759d83206bf936b938cbdb315437a6905d28a
Mario Pereira <mario.mpereira@gmail.com>
2013-03-20 11:04:19 +0100

    added parameters

--- src/main/java/com/genebio/nextprot/controllers/PublicationController.java
+++ src/main/java/com/genebio/nextprot/controllers/PublicationController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.genebio.nextprot.domain.Publication;
 import com.genebio.nextprot.service.PublicationService;
@@ -16,14 +18,17 @@ public class PublicationController {
 	@Autowired
 	PublicationService publicationService;
 	
-	@RequestMapping(value="/pub", method=RequestMethod.GET)
-	public Publication hello() {
-		return publicationService.getPublicationById(6634104);
+//	http://localhost:8080/spring-api/pub/6634104.xml
+	@RequestMapping(value="/pub/{id}", method=RequestMethod.GET)
+	public Publication get(@PathVariable("id") String id) {
+		return publicationService.getPublicationById(Integer.parseInt(id));
 	}
+
 	
 	
+//	http://localhost:8080/spring-api/list.xml?title=correction
 	@RequestMapping(value="/list", method=RequestMethod.GET)
-	public List<Publication> hell() {
-		return publicationService.getPublicationByTitle(null);
+	public List<Publication> list(@RequestParam("title") String title) {
+		return publicationService.getPublicationByTitle("%"+title+"%");
 	}
 }
